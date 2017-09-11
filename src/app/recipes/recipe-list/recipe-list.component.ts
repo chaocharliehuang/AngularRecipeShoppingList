425f00a91b04f60deb158ab444568011eebcaac1
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-09-10 21:23:28 -0700

    created all recipes and shopping-list components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this is a test', 'http://lorempixel.com/output/food-q-c-640-480-2.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
