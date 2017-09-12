a5aa4c0f6a41e73df2abd11a6a5db78aefb15d4a
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-09-11 20:15:49 -0700

    added recipe items and shopping list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'this is a test', 'http://lorempixel.com/output/food-q-c-640-480-2.jpg')
+    new Recipe('Test Recipe', 'this is a test', 'http://lorempixel.com/output/food-q-c-640-480-2.jpg'),
+    new Recipe('Test Recipe', 'this is a test', 'http://lorempixel.com/output/food-q-c-640-480-1.jpg'),
   ];
 
   constructor() { }
